88dddb08a045a7407f7b81ee0b7a1434d163275b
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-04-05 22:19:51 +0530

    build halsensor from source with wrapper

--- Android.mk
+++ Android.mk
@@ -20,4 +20,24 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
+ifeq ($(USE_MTK_CAMERA_WRAPPER),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam2halsensor
+LOCAL_SRC_FILES_64 := lib64/libcam2halsensor.so
+LOCAL_SRC_FILES_32 := lib/libcam2halsensor.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcameracustom
+LOCAL_SRC_FILES_64 := lib64/libcameracustom.so
+LOCAL_SRC_FILES_32 := lib/libcameracustom.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+endif
+
 endif
